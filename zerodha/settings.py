bf293c9787f8fcedfe57ce2460362a585f6e3395
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2021-05-14 17:17:20 +0530

    config files for celery

--- zerodha/settings.py
+++ zerodha/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--rk#rbyu^x=ref%wfwn!l&rszwg#rd3h_7k$kj_t47qh*(skbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
